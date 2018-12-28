edc31aeeb846e8d73be7d8e1c487046a296ea525
StoneT2000 <stoneztao@gmail.com>
2018-12-28 09:52:27 +0800

    Fixed CSS

--- HTML-Templates/BasicCleanPage (Small footer Hidden 60px reach)/scripts/base.js
+++ HTML-Templates/BasicCleanPage (Small footer Hidden 60px reach)/scripts/base.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
   $( '.footer_fascade' ).mouseenter(
     function() {
-      $('.footer').css('transform','translateY(0px)');
+      $('.footer').css('transform','translateY(5px)');
     }
   ).mouseleave(function(){
-    $('.footer').css('transform','translateY(0px)');
+    $('.footer').css('transform','translateY(60px)');
   });
   $('.footer').mouseenter(function() {
-    $('.footer').css('transform','translateY(0px)');
+    $('.footer').css('transform','translateY(5px)');
   })
   $('.footer').mouseleave(function() {
       $('.footer').css('transform','translateY(60px)');
